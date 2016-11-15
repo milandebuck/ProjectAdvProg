78c79cee4b42f58acd582abb53b7222078998eeb
Domien Herdewel <domien.herdewel@student.odisee.be>
2016-11-15 11:22:07 +0100

    update on db.java

--- Java Backend/src/main/java/db/Db.java
+++ Java Backend/src/main/java/db/Db.java
@@ -1,36 +1,24 @@
-import com.mongodb.BasicDBObject;
-import com.mongodb.BulkWriteOperation;
-import com.mongodb.BulkWriteResult;
-import com.mongodb.Cursor;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
-import com.mongodb.ParallelScanOptions;
-import com.mongodb.ServerAddress;
+package db;
 
-import java.util.List;
-import java.util.Set;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class DbFiller{
-    public static void main(string[] args){
+public class Db{
+    public static void main(String[] args){
         //Make database
-        MongoCredential credential = MongoCredential.createCredential(user1, translator_db, Azerty123);
+        /*MongoCredential credential = MongoCredential.createCredential(user1, ProjectDB, Azerty123);
         MongoClient mongoClient = new MongoClient("localhost", Arrays.asList(credential));
-        Db db = mongoClient.getDB("translator_db");
-        DbCollection collection = db.getCollection("en_nl_list");
+        Db db = mongoClient.getDB("ProjectDB");
+        DbCollection collection = db.getCollection("en_nl_list");*/
 
         //Read file
-        BufferedReader br = null;
+        BufferedReader br;
         try {
             String currentline;
             int count = 0;
-            String wordNL = "";
-            String wordEN = "";
+            String wordNL;
+            String wordEN;
             br = new BufferedReader(new FileReader("/data/output.txt"));
             while ((currentline = br.readLine()) != null) {
                 count += 1;
@@ -39,11 +27,14 @@ public class DbFiller{
                 }
                 else{
                     wordEN = currentline;
-                    BasicDBObject doc = new BasicDBObject("en", wordEN)
+                    /*BasicDBObject doc = new BasicDBObject("en", wordEN)
                             .append("nl", wordNL);
-                    collection.insert(doc);
+                    collection.insert(doc);*/
                 }
             }
+        }
+        catch(IOException ex){
+            System.out.print(ex.getMessage());
         }
     }
 }
