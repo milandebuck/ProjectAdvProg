b32d91942cae25af4f7e10007cf2114194892a60
Robbe De Geyndt <robbe.degeyndt@student.odisee.be>
2016-11-19 15:16:59 +0100

    MongoDB Credentials fix

--- Java Backend/src/main/java/db/SpringMongoConfig.java
+++ Java Backend/src/main/java/db/SpringMongoConfig.java
@@ -1,16 +1,19 @@
 package db;
 
-import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import java.util.Arrays;
 
- // The Database Configuration
+
+// The Database Configuration
 
 @EnableMongoRepositories(basePackages="mongodb.repository")
 @Configuration
@@ -18,8 +21,9 @@ public class SpringMongoConfig {
 
     @Bean
     public MongoDbFactory mongoDbFactory() throws Exception {
-        UserCredentials userCredentials = new UserCredentials("user1", "Azerty123");
-        return new SimpleMongoDbFactory(new Mongo(), "TeamMartini", userCredentials);
+        MongoCredential userCredentials = MongoCredential.createScramSha1Credential("user1","TeamMartini", "Azerty123".toCharArray());
+        MongoClient client = new MongoClient(new ServerAddress("localhost", 27017), Arrays.asList(userCredentials));
+        return new SimpleMongoDbFactory(client,"TeamMartini");
     }
 
     @Bean
