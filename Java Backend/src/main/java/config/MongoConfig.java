890f4fff02d2bf5a15ddf7c5ab0a39238d7fe3b4
Milan De Buck <milan.debuck@student.odisee.be>
2016-11-22 09:43:14 +0100

    added mongocredentials to the config

--- Java Backend/src/main/java/config/MongoConfig.java
+++ Java Backend/src/main/java/config/MongoConfig.java
@@ -2,9 +2,14 @@ package main.java.config;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by milan on 21.11.16.
@@ -20,7 +25,14 @@ public class MongoConfig extends AbstractMongoConfiguration {
 
     @Override
     public Mongo mongo() throws Exception {
-        return new MongoClient("localhost", 27017);
+        List<MongoCredential> credentials = new ArrayList<MongoCredential>();
+        credentials.add(
+                MongoCredential.createMongoCRCredential(
+                        "user1",
+                        "TeamMartini",
+                        "Azerty123".toCharArray()
+                ));
+        return new MongoClient(new ServerAddress("localhost", 27017),credentials);
     }
 
     @Override
