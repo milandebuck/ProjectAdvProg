24c63047b0f5f2137cd941dd93073520d77c8c98
Milan De Buck <milan.debuck@student.odisee.be>
2016-11-28 14:49:50 +0100

    added redirect to login page

--- AngularFrontend/src/app.module.ts
+++ AngularFrontend/src/app.module.ts
@@ -5,18 +5,25 @@ import { RouterModule }   from '@angular/router';
 
 import {
     AppComponent,
+    LoginComponent
 } from './components';
 
 import { routes } from './app.routes';
 
+//services
+
+import { UserService } from './services/user.service';
+import { LoggedInGuard } from './services/logged-in.guard';
+
+
 @NgModule({
     bootstrap: [ AppComponent ],
-    declarations: [ AppComponent ],
+    declarations: [ AppComponent,LoginComponent ],
     imports: [
         BrowserModule,
         HttpModule,
         RouterModule.forRoot(routes)
     ],
-    providers: [  ]
+    providers: [ UserService,LoggedInGuard ]
 })
 export class AppModule {}
--- AngularFrontend/src/app.routes.ts
+++ AngularFrontend/src/app.routes.ts
@@ -1,5 +1,9 @@
-import { LoginComponent } from './components/LoginComponent'
+import { LoginComponent } from './components'
+
+import { LoggedInGuard } from './services/logged-in.guard';
 
 export const routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
+    //{ path: 'profile', component: ProfileComponent, canActivate: [LoggedInGuard] }
 ];
