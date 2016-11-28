afa76ef7152c4b30cc81b47d651a3e41ac87540a
Domien Herdewel <domien.herdewel@student.odisee.be>
2016-11-28 23:10:58 +0100

    html for excercise

---
+++ AngularFrontend/src/app.module.js
@@ -0,0 +1,40 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var http_1 = require('@angular/http');
+var platform_browser_1 = require('@angular/platform-browser');
+var router_1 = require('@angular/router');
+var forms_1 = require('@angular/forms');
+var components_1 = require('./components');
+var app_routes_1 = require('./app.routes');
+//services
+var user_service_1 = require('./services/user.service');
+var logged_in_guard_1 = require('./services/logged-in.guard');
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        core_1.NgModule({
+            bootstrap: [components_1.AppComponent],
+            declarations: [components_1.AppComponent, components_1.LoginComponent],
+            imports: [
+                platform_browser_1.BrowserModule,
+                http_1.HttpModule,
+                forms_1.FormsModule,
+                router_1.RouterModule.forRoot(app_routes_1.routes)
+            ],
+            providers: [user_service_1.UserService, logged_in_guard_1.LoggedInGuard]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppModule);
+    return AppModule;
+})();
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
---
+++ AngularFrontend/src/app.routes.js
@@ -0,0 +1,6 @@
+var components_1 = require('./components');
+exports.routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: components_1.LoginComponent },
+];
+//# sourceMappingURL=app.routes.js.map
