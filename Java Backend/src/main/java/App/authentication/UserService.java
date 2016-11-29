6b68db6a6cac7d33fdde6afda7296bdab02b2733
Domien Herdewel <domien.herdewel@student.odisee.be>
2016-11-29 23:41:07 +0100

    Revert "Update UserService.java"
    This reverts commit 99fdb04f82b108b0562a96768e893bfce3b34353.

--- Java Backend/src/main/java/App/authentication/UserService.java
+++ Java Backend/src/main/java/App/authentication/UserService.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.test.context.ContextConfiguration;
 
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 
