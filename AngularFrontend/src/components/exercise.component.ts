67adae0f9fc1409fa6939b946072e40433d33686
Milan De Buck <milan.debuck@student.odisee.be>
2016-11-29 15:52:34 +0100

    added html for exercisecomponent

--- AngularFrontend/src/components/exercise.component.ts
+++ AngularFrontend/src/components/exercise.component.ts
@@ -14,7 +14,27 @@ import  { EntryService } from  './../services/entry.service';
     styles: [ require('./styles/exercise.component.css') ]
 })
 export class ExerciseComponent {
+    public count = 0;
+    public lenght = 10;
+    private answers = [];
+
     constructor( private entryService : EntryService){};
 
-    entries=this.entryService.getEntries(10);
+    entries=this.entryService.getEntries(this.lenght);
+
+    next(answer){
+        this.answers.push(answer);
+        if(this.count > this.lenght)this.count++;
+        else{
+            this.correctExercise().subscribe((res) => {
+               if(res){
+
+               }
+            });
+        }
+    }
+
+    private correctExercise(){
+        
+    }
 }
