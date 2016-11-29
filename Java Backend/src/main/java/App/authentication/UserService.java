99fdb04f82b108b0562a96768e893bfce3b34353
robbedg <robbedg@users.noreply.github.com>
2016-11-29 17:00:10 +0100

    Update UserService.java

--- Java Backend/src/main/java/App/authentication/UserService.java
+++ Java Backend/src/main/java/App/authentication/UserService.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.test.context.ContextConfiguration;
 
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 
