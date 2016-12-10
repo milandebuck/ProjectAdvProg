d33f36a1f9706f13a37a109b2d7dc8e35a1cca9b
robbedg <robbedg@users.noreply.github.com>
2016-12-10 14:05:25 +0100

    Update WebSecurityConfig.java

--- Java Backend/src/main/java/App/authentication/WebSecurityConfig.java
+++ Java Backend/src/main/java/App/authentication/WebSecurityConfig.java
@@ -64,7 +64,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable() // disable csrf for our requests.
                 .authorizeRequests()
-                .antMatchers("/", "/login", "/registration", "/Exercise", "/GetUser").permitAll()
+                .antMatchers("/", "/login", "/registration", "/Exercise").permitAll()
                 .anyRequest().authenticated();
 
         //If user is not authenticated / has no authorization, use the exceptionHandling.
