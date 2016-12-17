5cd60acd5196b90e42fda2e7472c807afaca4b47
Robbe De Geyndt <robbe.degeyndt@student.odisee.be>
2016-12-17 15:17:07 +0100

    Added doc.

--- Java Backend/src/main/java/App/GroupController.java
+++ Java Backend/src/main/java/App/GroupController.java
@@ -6,6 +6,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 /**
+ * Controller for managing groups.
+ * Everything in this controller is available on "/Group".
  * Created by Robbe De Geyndt on 14/12/2016.
  */
 @CrossOrigin
@@ -13,6 +15,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/Group")
 public class GroupController {
 
+    /**
+     * Creates a group, only available for teachers. [POST]
+     * Available on "/Group/SaveGroup".
+     * @param input name of the group ex: {"name" : "XXX"}
+     * @param token token for authentication
+     * @return confirmation
+     */
     @CrossOrigin
     @RequestMapping(value = "/SaveGroup", method = RequestMethod.POST)
     public Wrapper createGroup(@RequestBody String input, @RequestParam("token") String token) {
@@ -20,6 +29,14 @@ public class GroupController {
         return new GroupInteraction(user).CreateGroup(input);
     }
 
+    /**
+     * Publishes test in group. [GET]
+     * Available on "/Group/PublishTest".
+     * @param groupid id from group
+     * @param testid id from test
+     * @param token token for authentication
+     * @return confirmation
+     */
     @CrossOrigin
     @RequestMapping(value = "/PublishTest", method = RequestMethod.GET)
     public Wrapper publishTest(@RequestParam("groupid") String groupid, @RequestParam("testid") String testid, @RequestParam("token") String token) {
@@ -27,6 +44,14 @@ public class GroupController {
         return new GroupInteraction(user).PublishTest(testid, groupid);
     }
 
+    /**
+     * Gets results for a test from all students in a specified group. [GET]
+     * Available on "/Group/GetResults".
+     * @param groupid id from group
+     * @param testid id from test
+     * @param token token for authentication
+     * @return list of results ex: [{"student" : "XXX", "results" : [...]}, {...}]
+     */
     @CrossOrigin
     @RequestMapping(value = "/GetResults", method = RequestMethod.GET)
     public Wrapper testResults(@RequestParam("groupid") String groupid, @RequestParam("testid") String testid, @RequestParam("token") String token) {
@@ -34,6 +59,15 @@ public class GroupController {
         return new GroupInteraction(user).TestResults(testid, groupid);
     }
 
+
+    /**
+     * Add students to group. [POST]
+     * Available on "/Group/AddStudents".
+     * @param input List of ids of students ex: ["id1", "id2"]
+     * @param groupid id of group given as parameter in URL
+     * @param token token for authentication
+     * @return confirmation
+     */
     @CrossOrigin
     @RequestMapping(value = "/AddStudents", method = RequestMethod.POST)
     public Wrapper addStudents(@RequestBody String input, @RequestParam("groupid") String groupid, @RequestParam("token") String token) {
--- Java Backend/src/main/java/App/ObjectReturnController.java
+++ Java Backend/src/main/java/App/ObjectReturnController.java
@@ -6,12 +6,20 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 /**
+ * Controller for getting quick access to database.
+ * Available on "/".
  * Created by Robbe De Geyndt on 12/12/2016.
  */
 @CrossOrigin
 @RestController
 public class ObjectReturnController {
 
+    /**
+     * Gives back all names and ids from tests from user. [GET]
+     * Available on "/GetListNames".
+     * @param token token for authentication
+     * @return list of testnames and ids ex: [{"name" : "XXX", "id": "XXX"}, {...}]
+     */
     @CrossOrigin
     @RequestMapping(method = RequestMethod.GET, value = "/GetListNames")
     public Wrapper getListNames(@RequestParam("token") String token) {
@@ -19,13 +27,28 @@ public class ObjectReturnController {
         return new GetObject(user).listNames();
     }
 
+    /**
+     * Gives back specific test. [GET]
+     * Available on "/GetList".
+     * @param id id from test
+     * @param token token for authentication
+     * @return test ex: {"name" : "XXX", "list" : [...]}
+     */
     @CrossOrigin
     @RequestMapping(method = RequestMethod.GET, value = "/GetList")
-    public Wrapper getList(@RequestParam("id") String input, @RequestParam("token") String token) {
+    public Wrapper getList(@RequestParam("id") String id, @RequestParam("token") String token) {
         String user = SecurityContextHolder.getContext().getAuthentication().getName().toString();
-        return new GetObject(user).getList(input);
+        return new GetObject(user).getList(id);
     }
 
+    /**
+     * Gives back search results for user. [GET]
+     * Username does not need to be complete and search is case insensitive.
+     * Available on "/SearchUser".
+     * @param name string that user wants to search
+     * @param token token for authentication
+     * @return list of users that match the query ex: [{"username" : "XXX", "id" : "XXX"}, {...}]
+     */
     @CrossOrigin
     @RequestMapping(method = RequestMethod.GET, value = "/SearchUser")
     public Wrapper searchUser(@RequestParam("name") String name, @RequestParam("token") String token) {
